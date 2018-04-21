e00ac6ff65abe72c964b5c9f20967e4c74100144
marcel <diegomarcel27@hotmail.com>
2018-04-20 21:10:17 -0300

    ver 0.1

---
+++ Robot/Assets/FuerzasAngulares.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuerzasAngulares : MonoBehaviour {
+
+    public KeyCode left;
+    public KeyCode right;
+    public int angspeed;
+    Rigidbody rb;
+    private Vector3 direccion;
+    public Vector3 headPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        direccion = new Vector3(1, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(right))
+        {
+            rb.AddForceAtPosition(direccion * angspeed *-1 , headPosition);
+        }
+
+        if (Input.GetKey(left))
+        {
+            rb.AddForceAtPosition(direccion * angspeed, headPosition);
+        }
+    }
+}
---
+++ Robot/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public KeyCode left;
+    public KeyCode right;
+    public int speed;
+    public int angspeed;
+    Rigidbody rb;
+    private Vector3 direccion;
+    private Vector3 direccionAngular;
+    public Vector3 headPosition;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        direccion = new Vector3(1, 0, 0);
+        direccionAngular = new Vector3(1, 2, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(right)) {
+            rb.AddForce(direccion * speed);
+            rb.AddForceAtPosition(direccionAngular * angspeed * -1, headPosition);
+        }
+
+        if (Input.GetKey(left))
+        {
+            rb.AddForce(direccion * speed * -1);
+            rb.AddForceAtPosition(direccionAngular * angspeed, headPosition);
+        }
+    }
+}
